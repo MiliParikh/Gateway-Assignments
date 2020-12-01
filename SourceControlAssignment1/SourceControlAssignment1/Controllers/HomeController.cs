3d7d54f2cb030039f8335e832b58c1716e79a04f
MiliParikh <mili.parikh9@gmail.com>
2020-12-01 15:06:01 +0300

    Added the Source Control Assignment 1 files.

---
+++ SourceControlAssignment1/SourceControlAssignment1/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using SourceControlAssignment1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SourceControlAssignment1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Made by Mili Parikh";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Email ID: mili.parikh@thegatewaycorp.co.in";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult StudentDetails(student s)
+        {
+            if(ModelState.IsValid)
+            {
+                ViewBag.FirstName = s.FirstName;
+                ViewBag.MiddleName = s.MiddleName;
+                ViewBag.LastName = s.LastName;
+                ViewBag.Age = s.Age;
+                ViewBag.PhoneNumber = s.PhoneNumber;
+                ViewBag.Email = s.Email;
+                ViewBag.ConfirmEmail = s.ConfirmEmail;
+                ViewBag.College = s.College;
+                ViewBag.Branch = s.Branch;
+                return View("StudentDetails");
+            }
+            else
+            {
+                ViewBag.FirstName = "No Data";
+                ViewBag.MiddleName = "No Data";
+                ViewBag.LastName = "No Data";
+                ViewBag.Age = "No Data";
+                ViewBag.PhoneNumber = "No Data";
+                ViewBag.Email = "No Data";
+                ViewBag.ConfirmEmail = "No Data";
+                ViewBag.College = "No Data";
+                ViewBag.Branch = "No Data";
+                return View("StudentDetails");
+
+            }
+        }
+    }
+}
---
+++ SourceControlAssignment1/SourceControlAssignment1/Models/student.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SourceControlAssignment1.Models
+{
+    public class student
+    {
+
+        [Required(ErrorMessage = "First Name is Required")]
+        [StringLength(50, MinimumLength = 3)]
+        public string FirstName { get; set; }
+
+        [StringLength(50, MinimumLength = 1)]
+        public string MiddleName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is Required")]
+        [StringLength(50, MinimumLength = 3)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Age is Required")]
+        [Range(18, 40, ErrorMessage = "Age must be between 18-40 in years.")]
+        public int Age { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{2})[-. ]?([0-9]{4})[-. ]?([0-9]{3})[-. ]?([0-9]{3})$", ErrorMessage = "Not a valid Phone number (Enter in format : 91-1234-567-890")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Email ID is Required")]
+        [DataType(DataType.EmailAddress)]
+        [MaxLength(70)]
+        [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Incorrect Email Format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Confirm Email is Required")]
+        [DataType(DataType.EmailAddress)]
+        [System.ComponentModel.DataAnnotations.Compare("Email", ErrorMessage = "Email Not Matched")]
+        public string ConfirmEmail { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string College { get; set; }
+
+        [Required]
+        [StringLength(60)]
+        public string Branch { get; set; }
+}
+}
